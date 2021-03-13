8fdf78a14657ffea42bc4138df3834a359cd8d4d
Takayyz <like.the.wind0306@gmail.com>
2021-03-14 00:24:44 +0900

    add: practice trailing comma in parameters

--- index.php
+++ index.php
@@ -5,4 +5,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 // require_once __DIR__ . '/src/01_named_arguments.php';
 // require_once __DIR__ . '/src/02_union_types.php';
 // require_once __DIR__ . '/src/03_constructor_property_promotion.php';
-require_once __DIR__ . '/src/04_nullable_operator.php';
+// require_once __DIR__ . '/src/04_nullable_operator.php';
+require_once __DIR__ . '/src/05_trailing_comma_in_parameters.php';
---
+++ src/05_trailing_comma_in_parameters.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @param string $name
+ * @param int $age
+ */
+function hello(string $name, int $age,)
+{
+    echo "Hello {$name}!, Your age is {$age}";
+}
+
+hello(age: 30, name: 'Taka');
