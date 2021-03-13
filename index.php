db356457e974aef9cf96917482afeb50ee1f2679
Takayyz <like.the.wind0306@gmail.com>
2021-03-13 23:33:49 +0900

    add: practice named arguments

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/01_named_arguments.php';
---
+++ src/01_named_arguments.php
@@ -0,0 +1,20 @@
+<?php
+
+$numbers = [1, 2, 3, 4, 5, 6];
+
+// arrayとcallbackがarray_filterで定義されている引数名
+$evens = array_filter(array: $numbers, callback: function($num) {
+    return $num %  2 == 0;
+});
+
+// arrayとcallbackgがarray_mapで定義されている引数名
+// 本来array_mapは第一引数にcallback、第二引数にarrayを取るが、named argumentsの機能を利用して引数を入れ替え
+$squares = array_map(array: $numbers, callback: function($num) {
+    return $num * $num;
+});
+
+$pos = strpos(haystack: 'Hello World', needle: 'World');
+
+echo '<pre>';
+var_dump($pos, $evens, $squares);
+echo '</pre>';
